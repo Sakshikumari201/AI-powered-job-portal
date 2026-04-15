a0c871c055cefc7a9ad7571e4f45f818740074ed
Sakshi kumari 201 <sakshiparmarsingh456@gmail.com>
2026-04-15 22:45:42 +0530

    Fix: removed pino-pretty transport to prevent Netlify serverless crash

--- server/config/logger.js
+++ server/config/logger.js
@@ -2,16 +2,6 @@ const pino = require('pino');
 
 const logger = pino({
   level: process.env.LOG_LEVEL || 'info',
-  transport: (process.env.NODE_ENV === 'development' && !process.env.NETLIFY)
-    ? {
-        target: 'pino-pretty',
-        options: {
-          colorize: true,
-          translateTime: 'HH:MM:ss Z',
-          ignore: 'pid,hostname'
-        }
-      }
-    : undefined
 });
 
 module.exports = logger;
